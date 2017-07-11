010bed3496f08d9c456a085653d6a148b391507e
Jaycee Bernardino <jayceeb@clearstorydata.com>
2017-07-11 10:23:08 -0400

    Expire messages after 7 days

--- attributes/default.rb
+++ attributes/default.rb
@@ -29,7 +29,6 @@ default['activemq']['wrapper']['truststore_path'] = '%ACTIVEMQ_CONF%/broker.ts'
 
 default['activemq']['enabled'] = true
 default['activemq']['enable_stomp'] = true
-default['activemq']['dlq_process_expired'] = false
 default['activemq']['enable_broker_statistics'] = true
 default['activemq']['use_default_config'] = false
 default['activemq']['install_java'] = true
